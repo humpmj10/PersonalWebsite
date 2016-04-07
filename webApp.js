62236148b9a5c7b7d26317457b0623eb948f65b0
Michael Humphrey <michael.j.humphrey@gmail.com>
2016-04-07 13:46:42 -0700

    New website

---
+++ webApp.js
@@ -0,0 +1,19 @@
+var app = angular.module('phonecatApp', ['ngRoute', 'door3.css', 'ui.bootstrap']);
+  
+app.constant('baseUrl', 'file:///K:/bitbucket%20projects/Personal%20Website/') ;
+
+app .config(['$routeProvider', '$locationProvider', function($routeProvider, $locationProvider) {
+    $routeProvider.
+      when('/about', {
+        templateUrl: 'templates/bio.html',
+      }).
+      when('/resume', {
+        templateUrl: 'templates/resume.html',
+      }).
+      when('/home', {
+        templateUrl: 'templates/home.html',
+      }).
+      otherwise({
+        redirectTo: '/home'
+      });
+  }]);
